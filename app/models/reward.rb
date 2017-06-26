cb5cf9f26937ea242073651081c3991a4bbc0b89
Doug <doug.steinberg@gmail.com>
2017-06-26 16:03:13 -0400

    Added test for negative reward dollar amount

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,7 +1,7 @@
 class Reward < ActiveRecord::Base
   belongs_to :project
   validates :description, :dollar_amount, presence: true
-
+  validates :dollar_amount, numericality: { greater_than: 0 }
 end
 
 
