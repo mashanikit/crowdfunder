092829a30ae828b2282fa34cdacebf3858f7294f
Doug <doug.steinberg@gmail.com>
2017-06-26 15:46:13 -0400

    Added test for negative case of start date after end date

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -29,4 +29,15 @@ class ProjectTest < ActiveSupport::TestCase
 
     assert project.invalid?, 'Project should not save with start date in the past'
   end
+
+  test 'project is invalid with end date before start date' do
+    owner = build(:user, first_name: 'Sally', last_name: 'Lowenthal', email: 'sally@example.com', password:'passpass', password_confirmation: 'passpass')
+    project = build(:project, title: 'Cool new boardgame', description: 'Trade sheep', start_date:  Date.today + 2.month, end_date: Date.today + 1.month, goal: 50000)
+
+    project.owner = owner
+
+    assert project.invalid?, 'Project end date must be after its start date'
+  end
+
+
 end
