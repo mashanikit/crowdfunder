f4ac13706c997b43eb0f9c49a0ec45b5285c04d6
Doug <doug.steinberg@gmail.com>
2017-06-26 00:06:56 -0400

    Added factories to pledge test

--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -3,26 +3,13 @@ require_relative '../test_helper'
 class PledgeTest < ActiveSupport::TestCase
 
   test 'A pledge can be created' do
-    pledge = Pledge.create(
-      dollar_amount: 99.00,
-      project: new_project,
-      user: new_user
-    )
-    pledge.user = User.new
-    pledge.save
+    pledge = build(:pledge, dollar_amount: 99.00, project: new_project)
+    pledge.user= User.new
     assert pledge.valid?
-    assert pledge.persisted?
   end
 
   test 'owner cannot back own project' do
-    owner = new_user
-    owner.save
-    project = new_project
-    project.owner = owner
-    project.save
-    pledge = Pledge.new(dollar_amount: 3.00, project: project)
-    pledge.user = owner
-    pledge.save
+    pledge = build(:pledge, dollar_amount: 3.00, project: project, user: owner)
     assert pledge.invalid?, 'Owner should not be able to pledge towards own project'
   end
 
