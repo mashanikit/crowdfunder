d3517f6fc5cf7e01ddfe17103d8c66192f5d2661
Doug <doug.steinberg@gmail.com>
2017-06-26 00:52:01 -0400

    Factories built. All falures resolved. Coding in green

--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -14,10 +14,13 @@ class PledgeTest < ActiveSupport::TestCase
   test 'owner cannot back own project' do
     owner = build(:user, first_name: 'Sally', last_name: 'Lowenthal', email: 'sally@example.com', password:'passpass', password_confirmation: 'passpass')
     # owner.save
-    new_project = build(:project, title: 'Cool new boardgame', description: 'Trade sheep', start_date:  Date.today, end_date: Date.today + 1.month, goal: 50000)
+    project = build(:project, title: 'Cool new boardgame', description: 'Trade sheep', start_date:  Date.today, end_date: Date.today + 1.month, goal: 50000)
+    project.owner = owner
     # new_project.save
-    pledge = build(:pledge, dollar_amount: 3.00, project: new_project, user: owner)
+    pledge = build(:pledge, dollar_amount: 3.00, project: project, user: owner)
+    # pledge.save
     assert pledge.invalid?, 'Owner should not be able to pledge towards own project'
+
   end
 
 end
