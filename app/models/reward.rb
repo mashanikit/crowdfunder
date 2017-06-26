7f4ab89f4ce0cec776ff6b90361614475a1c4d89
Doug <doug.steinberg@gmail.com>
2017-06-26 16:19:16 -0400

    Deleted old comment

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -3,8 +3,3 @@ class Reward < ActiveRecord::Base
   validates :description, :dollar_amount, presence: true
   validates :dollar_amount, numericality: { greater_than: 0 }
 end
-
-
-
-
-# Reward dollar_amount must be positive number
