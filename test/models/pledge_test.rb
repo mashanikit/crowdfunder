92c6d480f20b616e71ad6d0aa92aa37d2a1b98ca
Doug <doug.steinberg@gmail.com>
2017-06-26 00:48:01 -0400

    Factories added to all tests. Currently pledge test failure

--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -19,25 +19,5 @@ class PledgeTest < ActiveSupport::TestCase
     pledge = build(:pledge, dollar_amount: 3.00, project: new_project, user: owner)
     assert pledge.invalid?, 'Owner should not be able to pledge towards own project'
   end
-  #
-  # def new_project
-  #   Project.new(
-  #     title:       'Cool new boardgame',
-  #     description: 'Trade sheep',
-  #     start_date:  Date.today,
-  #     end_date:    Date.today + 1.month,
-  #     goal:        50000
-  #   )
-  # end
-  #
-  # def new_user
-  #   User.new(
-  #     first_name:            'Sally',
-  #     last_name:             'Lowenthal',
-  #     email:                 'sally@example.com',
-  #     password:              'passpass',
-  #     password_confirmation: 'passpass'
-  #   )
-  # end
 
 end
