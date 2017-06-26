84fe60aa9e8341543c15c48b27fbfa40e14cfa63
Doug <doug.steinberg@gmail.com>
2017-06-26 00:41:20 -0400

    Changes to pledge test factories. 2 failures currently

--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -3,34 +3,41 @@ require_relative '../test_helper'
 class PledgeTest < ActiveSupport::TestCase
 
   test 'A pledge can be created' do
+    new_project = build(:project, title: 'Cool new boardgame', description: 'Trade sheep', start_date:  Date.today, end_date: Date.today + 1.month, goal: 50000)
+    # new_project.save
     pledge = build(:pledge, dollar_amount: 99.00, project: new_project)
+    # pledge.save
     pledge.user= User.new
     assert pledge.valid?
   end
 
   test 'owner cannot back own project' do
-    pledge = build(:pledge, dollar_amount: 3.00, project: project, user: owner)
+    owner = build(:user, first_name: 'Sally', last_name: 'Lowenthal', email: 'sally@example.com', password:'passpass', password_confirmation: 'passpass')
+    # owner.save
+    new_project = build(:project, title: 'Cool new boardgame', description: 'Trade sheep', start_date:  Date.today, end_date: Date.today + 1.month, goal: 50000)
+    # new_project.save
+    pledge = build(:pledge, dollar_amount: 3.00, project: new_project, user: owner)
     assert pledge.invalid?, 'Owner should not be able to pledge towards own project'
   end
-
-  def new_project
-    Project.new(
-      title:       'Cool new boardgame',
-      description: 'Trade sheep',
-      start_date:  Date.today,
-      end_date:    Date.today + 1.month,
-      goal:        50000
-    )
-  end
-
-  def new_user
-    User.new(
-      first_name:            'Sally',
-      last_name:             'Lowenthal',
-      email:                 'sally@example.com',
-      password:              'passpass',
-      password_confirmation: 'passpass'
-    )
-  end
+  #
+  # def new_project
+  #   Project.new(
+  #     title:       'Cool new boardgame',
+  #     description: 'Trade sheep',
+  #     start_date:  Date.today,
+  #     end_date:    Date.today + 1.month,
+  #     goal:        50000
+  #   )
+  # end
+  #
+  # def new_user
+  #   User.new(
+  #     first_name:            'Sally',
+  #     last_name:             'Lowenthal',
+  #     email:                 'sally@example.com',
+  #     password:              'passpass',
+  #     password_confirmation: 'passpass'
+  #   )
+  # end
 
 end
